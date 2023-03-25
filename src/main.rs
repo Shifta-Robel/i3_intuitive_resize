4f57de0d21a886e9fdc6183fb9b45f7ec5564798
Shifta-Robel <robelbire16@gmail.com>
2023-03-25 18:07:33 +0300

    Made program no longer depend on an ASSUMED_GAP_WIDTH

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use i3ipc::{reply::Node, I3Connection};
+use i3ipc::{reply::Node, reply::NodeType, I3Connection};
 use std::env;
 
 const STEP_COUNT: &str = "10 px or 10 ppt";
-const ASSUMED_MAX_INNER_GAP: u8 = 20;
 
 #[derive(Debug)]
 enum Motions {
@@ -23,6 +22,39 @@ fn find_focused(node: &Node) -> Option<&Node> {
             None
         }
     }
+}
+
+fn find_focused_workspace(node: &Node) -> Option<&Node> {
+    match node.nodetype {
+        NodeType::Workspace => Some(node),
+        _ => {
+            if let Some(&want) = node.focus.get(0) {
+                let child = node.nodes.iter().find(|n| want == n.id).unwrap();
+                find_focused_workspace(child)
+            } else {
+                None
+            }
+        }
+    }
+}
+
+fn find_extremes(node: &Node) -> (i32, i32, i32, i32) {
+    let mut extreme = node.rect;
+    for n in node.nodes.iter() {
+        if n.rect.0 < extreme.0 {
+            extreme.0 = n.rect.0
+        };
+        if n.rect.1 < extreme.1 {
+            extreme.1 = n.rect.1
+        };
+        if n.rect.0 + n.rect.2 > extreme.2 {
+            extreme.2 = n.rect.0 + n.rect.2
+        };
+        if n.rect.1 + n.rect.3 > extreme.3 {
+            extreme.3 = n.rect.1 + n.rect.3
+        };
+    }
+    extreme
 }
 
 fn get_args() -> (Motions, String) {
@@ -56,24 +88,18 @@ fn main() {
     let (move_to, move_by) = get_args();
     let mut connection = I3Connection::connect().expect("Failed to create connection");
     let tree = &connection.get_tree().expect("Failed to get Node");
-    let focused_node_rect = match find_focused(tree) {
+    let focused_workspace = find_focused_workspace(tree).expect("Failed to get focused workspace");
+    let extremes = find_extremes(focused_workspace);
+    let focused_node_rect = match find_focused(focused_workspace) {
         Some(node) => node,
         None => panic!("Failed to find focused node"),
     }
     .rect;
-    let w_spaces = connection
-        .get_workspaces()
-        .expect("Failed to get current workspaces");
-    let workspace_rect = w_spaces
-        .workspaces
-        .get(0)
-        .expect("No workspaces found")
-        .rect;
 
-    let left_corner = focused_node_rect.0 - workspace_rect.0 < ASSUMED_MAX_INNER_GAP.into();
-    let upper_corner = focused_node_rect.1 - workspace_rect.1 < ASSUMED_MAX_INNER_GAP.into();
-    let right_corner = focused_node_rect.0 + focused_node_rect.2 == workspace_rect.2;
-    let bottom_corner = focused_node_rect.1 + focused_node_rect.3 == workspace_rect.3;
+    let left_corner = focused_node_rect.0 == extremes.0;
+    let upper_corner = focused_node_rect.1 == extremes.1;
+    let right_corner = focused_node_rect.0 + focused_node_rect.2 == extremes.2;
+    let bottom_corner = focused_node_rect.1 + focused_node_rect.3 == extremes.3;
 
     let command: &str = match move_to {
         Motions::Up => {
